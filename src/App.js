0f14c4b6bebb4b03bc8c71fe53d373e067df6440
tbuddhi-dev <140359368+tbuddhi-dev@users.noreply.github.com>
2024-01-26 18:47:32 +0800

    Added all files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import IdentifyVoice from './IdentifyVoice'
+
+function App() {
+  return (
+    <div className="App">
+      <IdentifyVoice />
+    </div>
+  );
+}
+
+export default App;
